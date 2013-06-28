9effd2c6926d9420a58f1c1c0faac6d0dcc03bf7
Aaron Helton <abhelton@gmail.com>
2013-06-28 20:54:13 +0000

    still building voc.rb

--- voc.rb
+++ voc.rb
@@ -1,12 +1,77 @@
 #!/usr/bin/ruby
 # encoding: utf-8
 
+require 'tmpdir'
 
+# Variables that should probably come from argv
+thesaurus = "thesaurus.sdf.utf8"
+jsondir = "jsons"
+thesaurusmap = "thesaurus-map.json"
+skosfile = "thesaurus-skos.xml"
 
+# Global class for Authority Records
+class AuthorityRecord
+  def initialize()
+    @data = Hash.new { |hash, key| hash[key] = [] }
+  end
+  def [](key)
+    @data[key]
+  end
+  def []=(key,words)
+    @data[key] += [words].flatten
+    @data[key].uniq!
+  end
+end
 
 def jsonify(infile,outdir)
+  debug = 1
   # Transforms single SDF file to a collection of JSON formatted files representing each term.
-
+  # Check if infile exists and is readable.  Exit otherwise.
+  unless File.exists?(infile) && File.readable?(infile)
+    abort "File #{infile} does not exist or is not readable."
+  end
+  # Check if outdir exists, create it if necessary
+  if !Dir.exists?(outdir) 
+    Dir.mkdir(outdir) or abort "Unable to create the directory #{outdir}.  Perhaps you don't have permission."
+  end
+  # One more thing, can we make a tmpdir?
+  tmpdir = Dir.mktmpdir or abort "Could not make a temporary directory."
+  if debug == 1
+    puts tmpdir
+  end
+  # we should be good now, let's proceed
+  # First we make temporary SDFs, which we can discard later
+  i = 0
+  File.foreach(infile) do |line|
+    if line !~ /:/ 
+      if debug == 1
+        puts "Adding to file sdf-" + i.to_s + ".sdf"
+      end
+      i = i + 1
+    else
+      outfile = "#{tmpdir}/sdf-" + i.to_s + ".sdf"
+      File.open(outfile, "a") do |out|
+        out.puts line
+      end
+    end
+  end
+  # Next we jsonify these files and write them to the outdir
+  Dir.foreach(tmpdir) do |file|
+    if file != "." && file != ".."
+      fh = File.open("#{tmpdir}/#{file}", "rb")
+      authrecord = fh.read
+      fh.close
+      recordid = ""
+      authrecord.split("\n").each do |line|
+        if line =~ /Recordid/
+          recordid = line.split(": ")[1]
+        end
+        key = line.split(": ")[0]
+        value = line.split(": ")[1]
+      end
+      
+    end
+  end
 end
 
 def makemap(indir,mapfile)
@@ -16,3 +81,7 @@ end
 def skosify(indir,mapfile,outfile)
   # outputs SKOS Core formatted XML
 end
+
+jsonify(thesaurus,jsondir)
+
+
