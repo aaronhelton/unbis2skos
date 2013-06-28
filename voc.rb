34f0b90e8ad74efe8e5cf198656c594e0b9770ba
Aaron Helton <abhelton@gmail.com>
2013-06-28 15:40:38 +0000

    created voc.rb to contain major functions

---
+++ voc.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+
+
+
+
+def jsonify(infile,outdir)
+  # Transforms single SDF file to a collection of JSON formatted files representing each term.
+
+end
+
+def makemap(indir,mapfile)
+  # Makes JSON formatted map file(s) to lookup by T code and by ETerm
+end
+
+def skosify(indir,mapfile,outfile)
+  # outputs SKOS Core formatted XML
+end
